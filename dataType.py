7296ee3309ced11d192adcb882e0ee8f8f51a62f
刘剑 <1690296356@qq.com>
2024-05-12 17:25:55 +0800

    git:add 变量，数据类型

---
+++ dataType.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: thomasliu319@gmail.com
+@description: 数据类型测试案例
+    --用type()方法可以查看变量的类型
+@file: dataType.py
+@time: 2024/5/12 17:04
+"""
+# 定义变量
+a = 222
+b = 3.14
+c = "Python知识分享网"
+
+# 打印变量类型
+print("a=", a, type(a))
+print("b=", b, type(b))
+print("c=", c, type(c))
+
+
+
+
+
---
+++ variable.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: thomasliu319@gmail.com
+@description: 变量测试案例：
+    -- 定义变量
+    -- 输出变量显示变量值
+@file: helloWorld.py
+@time: 2024/5/12 16:41
+"""
+a = 222
+b = 3.14
+c = "Python知识分享网"
+print("a=", a)
+print(b)
+print(c)
+# 把（222-1）的结果，赋值给变量a
+a = 222 - 1
+# 打印变量
+print("a=", a)
